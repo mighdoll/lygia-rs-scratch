f0a8264542643822f0623995a435c47685d43441
lee mighdoll <lee@underneath.ca>
2025-06-08 14:12:19 -0700

    basic example, using random_wgsl

---
+++ build.rs
@@ -0,0 +1,6 @@
+fn main() {
+    #[cfg(feature = "build-time")]
+    wesl::Wesl::new("src/shaders")
+        .add_package(&random_wgsl::random::Mod)
+        .build_artefact("main", "main");
+}
---
+++ src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    // run wesl at build-time
+    #[cfg(feature = "build-time")]
+    let source = {
+        use wesl::include_wesl;
+        include_wesl!("main")
+    };
+
+    // run wesl at run-time
+    #[cfg(not(feature = "build-time"))]
+    let source = wesl::Wesl::new("src/shaders")
+        .add_package(&random_wgsl::random::Mod)
+        .compile("main")
+        .inspect_err(|e| {
+            eprintln!("{e}");
+            panic!();
+        })
+        .unwrap()
+        .to_string();
+
+    println!("{source}");
+}
