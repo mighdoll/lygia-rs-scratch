0c3393afda6251872fd4e0b4369e46bd95d22a15
lee mighdoll <lee@underneath.ca>
2025-06-08 20:15:39 -0700

    fix static build too

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() {
     #[cfg(feature = "build-time")]
     wesl::Wesl::new("src/shaders")
-        .add_package(&lygia::lygia::math::cubic::Mod)
+        .add_package(&lygia::lygia::Mod)
         .build_artifact("main", "main");
 }
