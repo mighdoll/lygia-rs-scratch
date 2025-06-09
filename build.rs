f464b81805597ff1879afac6852452717d7515c8
lee mighdoll <lee@underneath.ca>
2025-06-08 17:01:11 -0700

    add lygia dependency

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() {
     #[cfg(feature = "build-time")]
     wesl::Wesl::new("src/shaders")
-        .add_package(&random_wgsl::random::Mod)
-        .build_artefact("main", "main");
+        .add_package(&lygia::lygia::math::cubic::Mod)
+        .build_artifact("main", "main");
 }
